cf8211e23bd1f15a1aa37aedca3351e474f58d01
Nada Amin <namin@alum.mit.edu>
2013-01-17 11:14:26 +0100

    use released core.logic 0.8.0-rc2

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "explorations of core.logic"
   :url "https://github.com/namin/logically"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.logic "0.8.0-rc2-SNAPSHOT"]
+                 [org.clojure/core.logic "0.8.0-rc2"]
                  [org.clojure/core.match "0.2.0-alpha11"]])
