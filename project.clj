7c3344806a066c9dda35743d07b972d3658d9d12
Nada Amin <namin@alum.mit.edu>
2013-07-17 23:19:58 -0400

    update to latest version of libs

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "explorations of core.logic"
   :url "https://github.com/namin/logically"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.1"]
-                 [org.clojure/core.match "0.2.0-alpha12"]])
+                 [org.clojure/core.logic "0.8.3"]
+                 [org.clojure/core.match "0.2.0-rc4"]])
