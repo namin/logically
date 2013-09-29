7245f7148c700117f0bdb3d9c5e644d687af03bb
Nada Amin <namin@alum.mit.edu>
2013-09-29 22:08:25 +0200

    from scratch: a meta-encoding of lf in minikanren

--- test/logically/exp/lf1_test.clj
+++ test/logically/exp/lf1_test.clj
@@ -5,29 +5,38 @@
         [clojure.core.logic.nominal :exclude [fresh hash] :as nom])
   (:use [clojure.test]))
 
-(deftest test-nat
-  (is (= (run 3 [q] (nat-o q))
-         '(z (s z) (s (s z))))))
+(deftest test-naturals
+  (is (= (run* [q] (fresh [a b c] (== q [c a b]) (naturals-clauses c a b) (== a typ)))
+         '([nat typ []]
+           [(plus _0 _1 _2) typ [[_0 nat] [_1 nat] [_2 nat]]]
+           [(sum-inc _0 _1)
+            typ
+            [[_0 (plus _2 _3 _4)] [_1 (plus _2 (s _3) (s _4))]]])))
+  (is (= (run* [q] (fresh [a b c] (== q [c a b]) (naturals-clauses c a b)))
+         '([nat typ []]
+           [z nat []]
+           [(s _0) nat [[_0 nat]]]
+           [(plus _0 _1 _2) typ [[_0 nat] [_1 nat] [_2 nat]]]
+           [plus-z (plus z _0 _0) []]
+           [(plus-s _0) (plus (s _1) _2 (s _3)) [[_0 (plus _1 _2 _3)]]]
+           [(sum-inc _0 _1)
+            typ
+            [[_0 (plus _2 _3 _4)] [_1 (plus _2 (s _3) (s _4))]]]
+           [sum-inc-z (sum-inc plus-z plus-z) []]
+           [(sum-inc-s _0)
+            (sum-inc (plus-s _1) (plus-s _2))
+            [[_0 (sum-inc _1 _2)]]])))
+  (is (= (run 3 [q] (naturals q nat))
+         '(z (s z) (s (s z)))))
+  (is (= (run* [q] (fresh [n] (naturals q (plus z n n))))
+         '(plus-z)))
+  (is (= (run* [q] (naturals plus-z q))
+         '((plus z _0 _0))))
+  (is (= (run 3 [q] (fresh [a b c d] (== q [a b c d]) (naturals d (plus a b c))))
+         '([z _0 _0 plus-z]
+           [(s z) _0 (s _0) (plus-s plus-z)]
+           [(s (s z)) _0 (s (s _0)) (plus-s (plus-s plus-z))]))))
 
-(deftest test-plus
-  (is (= (run 3 [q] (fresh [n1 n2 n3 o]
-                           (plus-o n1 n2 n3 o)
-                           (== q [n1 n2 n3 o])))
-         '([z z z (plus-z z)]
-           [z (s z) (s z) (plus-z (s z))]
-           [z (s (s z)) (s (s z)) (plus-z (s (s z)))]))))
 
-(deftest test-plus-s
-  (is (= (run 2 [q] (fresh [n1 n2 n3 o oa]
-                           (conso 'plus-s oa o)
-                           (plus-o n1 n2 n3 o)
-                           (== q [n1 n2 n3 o])))
-         '([(s z) z (s z) (plus-s z z z (plus-z z))]
-           [(s z) (s z) (s (s z)) (plus-s z (s z) (s z) (plus-z (s z)))]))))
 
-(deftest test-sum-inc
-  (is (= (run 2 [q] (fresh [n1 n2 n3 p1 p2 o]
-                           (sum-inc-o n1 n2 n3 p1 p2 o)
-                           (== q [n1 n2 n3 p1 p2 o])))
-         '([z z z (plus-z z) (plus-z (s z)) (sum-inc-z z z z)]
-           [z z (s z) (plus-z z) (plus-z (s z)) (sum-inc-z z z (s z))]))))
+
