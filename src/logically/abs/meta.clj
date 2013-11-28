e6ae43ee7dcfeae5c84f4867d77538e25fc769f2
Nada Amin <namin@alum.mit.edu>
2013-11-28 11:26:40 +0100

    putt out ack norm ex

--- src/logically/abs/meta.clj
+++ src/logically/abs/meta.clj
@@ -3,39 +3,6 @@
   (:use [clojure.core.logic :exclude [is] :as l]
         [clojure.core.logic.nominal :exclude [fresh hash] :as nom]))
 
-(defn ack-clause [u== head body]
-  (conde
-   [(fresh [a b]
-           (== head [:== a b])
-           (u== a b)
-           (== body ()))]
-   [(fresh [a b c]
-           (== head [:ack a b c])
-           (conde
-            [(fresh [n]
-                    (== body [[:== a 0]
-                              [:== b n]
-                              [:== c [:s n]]]))]
-            [(fresh [m n d e f]
-                    (== body [[:== a [:s m]]
-                              [:== b 0]
-                              [:== c n]
-                              [:== d m]
-                              [:== e [:s 0]]
-                              [:== f n]
-                              [:ack d e f]]))]
-            [(fresh [d e f g h i m n o p]
-                    (== body [[:== a [:s m]]
-                              [:== b [:s n]]
-                              [:== c p]
-                              [:== d [:s m]]
-                              [:== e n]
-                              [:== f o]
-                              [:== g m]
-                              [:== h o]
-                              [:== i p]
-                              [:ack d e f] [:ack g h i]]))]))]))
-
 (defn solve* [u== clause goals]
   (conde
    [(== goals ())]
--- test/logically/abs/meta_test.clj
+++ test/logically/abs/meta_test.clj
@@ -1,6 +1,7 @@
 (ns logically.abs.meta_test
   (:use [logically.abs.meta] :reload)
   (:use [logically.abs.unif] :reload)
+  (:use [logically.abs.ex_ack] :reload)
   (:refer-clojure :exclude [==])
   (:use [clojure.core.logic :exclude [is] :as l]
         [clojure.core.logic.nominal :exclude [fresh hash] :as nom])
@@ -11,12 +12,12 @@
   (is (= (run 3 [q]
               (fresh [m n]
                      (== q [m n])
-                     (solve* u==-concrete ack-clause [[:ack [:s [:s 0]] n [:s m]]])))
+                     (solve* u==-concrete ack-norm-clause [[:ack [:s [:s 0]] n [:s m]]])))
          '([[:s [:s 0]] 0]
            [[:s [:s [:s [:s 0]]]] [:s 0]]
            [[:s [:s [:s [:s [:s [:s 0]]]]]] [:s [:s 0]]])))
   (is (= (run 3 [q]
-              (solve* u==-parity1 ack-clause [[:ack :even :one q]]))
+              (solve* u==-parity1 ack-norm-clause [[:ack :even :one q]]))
          '(:odd :odd :odd))))
 
 
