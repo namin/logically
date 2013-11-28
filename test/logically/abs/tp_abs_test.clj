0019856e1e674f7bb8c1c3cad624e786add929c9
Nada Amin <namin@alum.mit.edu>
2013-11-28 15:36:13 +0100

    add ackermann parity1 test

--- test/logically/abs/tp_abs_test.clj
+++ test/logically/abs/tp_abs_test.clj
@@ -2,6 +2,7 @@
   (:use [logically.abs.tp_abs] :reload)
   (:use [logically.abs.unif] :reload)
   (:use [logically.abs.ex_plus] :reload)
+  (:use [logically.abs.ex_ack] :reload)
   (:refer-clojure :exclude [==])
   (:use [clojure.core.logic :exclude [is] :as l]
         [clojure.core.logic.nominal :exclude [fresh hash] :as nom])
@@ -76,3 +77,25 @@
             [:times :even _0 :even]
             [:times :odd _0 _0]
             })))
+
+(deftest test-ack
+  (is (= (set (run* [q] (go u==-parity1 ack-norm-clause q)))
+         '#{
+            [:ack :zero :odd :even]
+            [:ack :zero :even :odd]
+            [:ack :zero :one :even]
+            [:ack :zero :zero :one]
+            [:ack :one :zero :even]
+            [:ack :one :one :odd]
+            [:ack :even :zero :odd]
+            [:ack :one :even :even]
+            [:ack :one :odd :odd]
+            [:ack :even :one :odd]
+            [:ack :odd :zero :odd]
+            [:ack :even :even :odd]
+            [:ack :even :odd :odd]
+            [:ack :odd :one :odd]
+            [:ack :odd :even :odd]
+            [:ack :odd :odd :odd]
+            }
+         )))
