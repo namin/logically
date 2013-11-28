3c1076dac75b9853a28cd7218cdd5a7ccd01fc38
Nada Amin <namin@alum.mit.edu>
2013-11-28 10:18:24 +0100

    add missing example

--- src/logically/abs/ex_rotate.clj
+++ src/logically/abs/ex_rotate.clj
@@ -14,3 +14,15 @@
    [(fresh [x xs ys zs]
            (== head [:append (lcons x xs) ys (lcons x zs)])
            (== body [[:append xs ys zs]]))]))
+
+(defn rotate-pp-clause [head body]
+  (conde
+   [(fresh [xs ys as bs]
+           (== head [:rotate xs ys])
+           (== body [[:pp 1 1 [:append as bs xs]] [:pp 1 2 [:append bs as ys]]]))]
+   [(fresh [ys]
+           (== head [:append () ys ys])
+           (== body []))]
+   [(fresh [x xs ys zs]
+           (== head [:append (lcons x xs) ys (lcons x zs)])
+           (== body [[:pp 3 1 [:append xs ys zs]]]))]))
