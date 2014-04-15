cca7450f171fb5a7194338a6b6c33bb52f22fb8c
Nada Amin <namin@alum.mit.edu>
2014-04-15 21:04:25 +0200

    Update deps, except clojure.
    Still use 1.5.1 instead of 1.6.0 because of issue with records being
    defined twice (once by core and once by core.logic).

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "explorations of core.logic"
   :url "https://github.com/namin/logically"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.5"]
-                 [org.clojure/core.match "0.2.0"]])
+                 [org.clojure/core.logic "0.8.7"]
+                 [org.clojure/core.match "0.2.1"]])
