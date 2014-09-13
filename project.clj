59fd94523d704951a2dda5a69a5afdda727feda7
Nada Amin <namin@alum.mit.edu>
2014-09-13 19:04:32 +0200

    update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject logically "0.1.3-SNAPSHOT"
   :description "explorations of core.logic"
   :url "https://github.com/namin/logically"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.7"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.logic "0.8.8"]
                  [org.clojure/core.match "0.2.1"]])
