3ad2a41bcaf13de339ad9756dc1076b682370594
Nada Amin <na482@cl.cam.ac.uk>
2018-01-24 00:22:33 +0000

    update deps

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "explorations of core.logic"
   :url "https://github.com/namin/logically"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.logic "0.8.10"]
-                 [org.clojure/core.match "0.3.0-alpha4"]])
+                 [org.clojure/core.logic "0.8.11"]
+                 [org.clojure/core.match "0.3.0-alpha5"]])
