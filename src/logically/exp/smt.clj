3c96eebb3f546bf0b5ddc0fdf58376110777527e
Nada Amin <na482@cl.cam.ac.uk>
2018-09-07 19:26:21 +0100

    implement manual purging

--- src/logically/exp/smt.clj
+++ src/logically/exp/smt.clj
@@ -47,6 +47,37 @@
     (and (check-sat ys)
          (get-model ys))))
 
+(defn add-model [m s]
+  (fn [a]
+    (if (empty? m)
+      a
+      (bind
+       ((== (.valAt s (first (first m))) (second (first m)))
+        a)
+       (add-model (rest m) s)))))
+
+(defn smt-purge-loop [ms smt-lines s a--]
+  (let [m (get-next-model smt-lines ms)]
+    (when m
+      (mplus
+       ((add-model m s) a--)
+       (fn [] (smt-purge-loop (cons m ms) smt-lines s a--))))))
+
+(defn smt-purge [a]
+  (let [cs (:cs a)
+        cm (:cm cs)
+        rs (vals cm)
+        xs (into [] (set (mapcat (fn [r] (-rands r)) rs)))
+        r (-reify* (with-meta empty-s (meta s)) xs)
+        s (reduce (fn [m x] (assoc m (walk r x) x)) {}  xs)
+        rr (map (fn [x] (-reifyc x nil r a)) rs)
+        xr (map (fn [x] (walk r x)) xs)
+        smt-lines (concat (map (fn [x] `(~'declare-const ~x ~'Int)) xr)
+                          (map (fn [x] `(~'assert ~x)) rr))
+        a-- (reduce (fn [a x] ((remcg x) a)) a rs)]
+    (when (check-sat smt-lines)
+      (smt-purge-loop '() smt-lines s a--))))
+
 (defn -smtc [xs p]
   (reify
     IConstraintStep
@@ -87,4 +118,9 @@
   (smtc [q] `(~'= ~q 3)))
 
 (run* [q]
-  (smtc [q] `(~'= ~q 1)))
+  (smtc [q] `(~'= ~q 1))
+  smt-purge)
+
+(run 2 [q]
+  (smtc [q] `(~'> ~q 0))
+  smt-purge)
