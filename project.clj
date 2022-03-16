323ed034dc93fcbc77241fb445c9ceeba93e2555
Nada Amin <namin@seas.harvard.edu>
2022-03-16 16:26:22 -0400

    update to latest deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject logically "0.1.4-SNAPSHOT"
   :description "explorations of core.logic"
   :url "https://github.com/namin/logically"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.logic "0.8.11"]
-                 [org.clojure/core.match "0.3.0"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.logic "1.0.1"]
+                 [org.clojure/core.match "1.0.0"]])
